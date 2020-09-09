08367df52642f71eb06b041918926e8ff2549b99
JongMyeong Jeong <jmjeong1112@gmail.com>
2020-09-09 18:32:03 +0900

    컴포넌트 여러 개 추가하기 위해 App.js 수정

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
+function Movie({ name, picture }) {
+  return (
+  <div>
+    <h2>I love { name }</h2>
+    <img src={ image } />
+  </div>
+  );
+}
+
+const moviesILike = [
+  {
+    name: "Iron Man",
+    image: "https://images-na.ssl-images-amazon.com/images/I/71lVAGaqBtL._AC_SY879_.jpg"
+  },
+  {
+    name: "Captain America",
+    image: "https://www.bestmovieposters.co.uk/wp-content/uploads/2019/01/9j2Fy3gE-1.jpeg"
+  },
+  {
+    name: "Spider Man",
+    image: "https://images-na.ssl-images-amazon.com/images/I/51UL0SttePL._AC_.jpg"
+  },
+  {
+    name: "Avengers",
+    image: "https://images-na.ssl-images-amazon.com/images/I/A1t8xCe9jwL._AC_SL1500_.jpg"
+  },
+];
+
 function App() {
-  return <div className="App" />;
+  return (
+  <div>
+    {moviesILike.map(movie => (
+      <Movie name={movie.name} picture={movie.image} />
+    ))}
+  </div>
+  );
 }
 
 export default App;
