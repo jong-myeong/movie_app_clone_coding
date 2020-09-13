76353c12d5b33bb41a06ab70d0d7366966344108
JONGMYEONG JEONG <jmjeong1112@gmail.com>
2020-09-13 16:30:58 +0900

    Movie 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
@@ -12,7 +13,7 @@ class App extends React.Component {
       data: {
         data: { movies },
       },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies_json");
+    } = await axios.get('https://yts-proxy.now.sh/list_movies_json?sort_by=rating');
     this.setState({ movies, isLoading: false })
   };
 
@@ -21,8 +22,31 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{ isLoading ? 'Loading...' : 'We are ready'}</div>
+    const { isLoading, movies } = this.state;
+    return (
+    <section class="container">
+      { isLoading ? ( 
+        <div class="loader">
+          <span class="loader__text">Loading...</span>
+        </div>
+      ) : (
+        <div class="movies">
+          {movies.map((movie) => {
+            return (
+              <Movie 
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            )
+          })}
+        </div>
+      )}
+    </section>
+    )
   }
 }
 
