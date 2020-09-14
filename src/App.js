daa5ef83f78fecf8747d8d6549717f5d173e6e18
JongMyeong Jeong <jmjeong1112@gmail.com>
2020-09-14 18:01:39 +0900

    Detail 컴포넌트 작성 & 리다이렉트 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-// import Home from './routes/Home'; ⇒ 잠시 삭제
 import './App.css'
 import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   )
 }
